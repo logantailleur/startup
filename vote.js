81bc7aec654409b35763484fc421825f70643178
logantailleur <logantailleur@gmail.com>
2023-03-04 16:45:29 -0700

    Debug for missing values in forms

--- vote.js
+++ vote.js
@@ -7,15 +7,66 @@ function vote() {
                 restaurant = radio.value;
             }
         });
-
-    updateResults(restaurant);
-    saveUserVotes(nameEl.value, restaurant);
-
-    // window.location.href = "results.html";
+    if (nameEl.value != ""){
+        saveUserVotes(nameEl.value, restaurant);
+        updateResults();
+    
+        window.location.href = "results.html";
+    }
 }
 
-function updateResults(restaurant) {
+function updateResults() {
+    let results = [];
+    const resultsText = localStorage.getItem('results');
+    if (resultsText) {
+        results = JSON.parse(resultsText);
+    }
+    let votes = [];
+    const voteResults = localStorage.getItem('userVotes');
+    if (voteResults) {
+        votes = JSON.parse(voteResults);
+    }
+    results = this.updateRestResults(results, votes);
+    localStorage.setItem('resResults', JSON.stringify(results));
+}
 
+function updateRestResults(results, votes) {
+    let newResults = [];
+    var newLine = {Restaurant: "McDonald's", Votes: getVotes(votes, "McDonald's")};
+    newResults.push(newLine);
+
+    newLine = {Restaurant: "Zaxby's", Votes: getVotes(votes, "Zaxby's")};
+    newResults.push(newLine);
+
+    newLine = {Restaurant: "KFC", Votes: getVotes(votes, "KFC")};
+    newResults.push(newLine);
+
+    newLine = {Restaurant: "Sonic", Votes: getVotes(votes, "Sonic")};
+    newResults.push(newLine);
+
+    newLine = {Restaurant: "JCW's", Votes: getVotes(votes, "JCW's")};
+    newResults.push(newLine);
+
+    newLine = {Restaurant: "Popeye's", Votes: getVotes(votes, "Popeye's")};
+    newResults.push(newLine);
+
+    newLine = {Restaurant: "Raising Cane's", Votes: getVotes(votes, "Raising Cane's")};
+    newResults.push(newLine);
+
+    newLine = {Restaurant: "Chick-fil-a", Votes: getVotes(votes, "Chick-fil-a")};
+    newResults.push(newLine);
+
+    return newResults;
+}
+
+function getVotes(votes, restName){
+    var total = 0;
+    for(const [i, prev] of votes.entries()) {
+        if (prev.Restaurant === restName) {
+            total = total + 1;
+        }
+    }
+    return total;
 }
 
 function saveUserVotes(userName, restaurant) {
@@ -25,6 +76,7 @@ function saveUserVotes(userName, restaurant) {
         results = JSON.parse(resultsText);
     }
     results = this.updateUserVotes(userName, restaurant, results);
+    // results = null;
     localStorage.setItem('userVotes', JSON.stringify(results));
 }
 
@@ -45,10 +97,6 @@ function updateUserVotes(userName, restaurant, results) {
     }
     if (!found) {
         newTable.push(newResult);
-    }
-
-    if (newTable.length > 10) {
-        newTable.length = 10;
     }
 
     return newTable;
