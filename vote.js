455bb6d8492803d5104992fe8894e22fb8573167
logantailleur <logantailleur@gmail.com>
2023-03-04 16:03:51 -0700

    Get user votes working

--- vote.js
+++ vote.js
@@ -1,22 +1,55 @@
 function vote() {
-    const nameEl = document.querySelector("#name");
-    if (nameEl.value != null) {
-        localStorage.setItem("userName", nameEl.value);
-    } else {
-        localStorage.setItem("userName", "anonymous");
+    var nameEl = document.querySelector("#inputName");
+    var restaurant = null;
+    document.getElementsByName('voteButton')
+        .forEach(radio => {
+            if (radio.checked) {
+                restaurant = radio.value;
+            }
+        });
+
+    updateResults(restaurant);
+    saveUserVotes(nameEl.value, restaurant);
+
+    // window.location.href = "results.html";
+}
+
+function updateResults(restaurant) {
+
+}
+
+function saveUserVotes(userName, restaurant) {
+    let results = [];
+    const resultsText = localStorage.getItem('userVotes');
+    if (resultsText) {
+        results = JSON.parse(resultsText);
     }
-    const restaurant = document.querySelector('.checkbox').checked;
-
-    updateResults(nameEl, restaurant);
-    updateUserVotes(nameEl, restaurant);
-
-    window.location.href = "results.html";
+    results = this.updateUserVotes(userName, restaurant, results);
+    localStorage.setItem('userVotes', JSON.stringify(results));
 }
 
-function updateResults(nameEl, restaurant){
+function updateUserVotes(userName, restaurant, results) {
+    const newResult = { User: userName, Restaurant: restaurant };
+    const newTable = [];
 
-}
+    let found = false;
+    if (results != null) {
+        for (const [i, prevResult] of results.entries()) {
+            if (userName === prevResult.User) {
+                newTable.push(newResult);
+                found = true;
+            } else {
+                newTable.push(prevResult);
+            }
+        }
+    }
+    if (!found) {
+        newTable.push(newResult);
+    }
 
-function updateUserVotes(nameEl, restaurant) {
-    
+    if (newTable.length > 10) {
+        newTable.length = 10;
+    }
+
+    return newTable;
 }
